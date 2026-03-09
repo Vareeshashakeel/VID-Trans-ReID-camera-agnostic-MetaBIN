dd2f7679abbdd10c2f5cc4d1e2e3e6487add29be
Vareeshashakeel <150204666+Vareeshashakeel@users.noreply.github.com>
2026-03-09 17:49:06 +0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -9,15 +9,6 @@ from typing import Dict, Any
 
 
 class RandomIdentitySampler(Sampler):
-    """
-    Randomly sample N identities, then for each identity,
-    randomly sample K instances, therefore batch size is N*K.
-    Args:
-    - data_source (list): list of (img_path, pid, camid).
-    - num_instances (int): number of instances per identity in a batch.
-    - batch_size (int): number of examples in a batch.
-    """
-
     def __init__(self, data_source, batch_size, num_instances):
         self.data_source = data_source
         self.batch_size = batch_size
@@ -29,7 +20,6 @@ class RandomIdentitySampler(Sampler):
             self.index_dic[pid].append(index)
         self.pids = list(self.index_dic.keys())
 
-        # estimate number of examples in an epoch
         self.length = 0
         for pid in self.pids:
             idxs = self.index_dic[pid]
@@ -72,8 +62,6 @@ class RandomIdentitySampler(Sampler):
 
 
 class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
     def __init__(self):
         self.reset()
 
@@ -91,17 +79,6 @@ class AverageMeter(object):
 
 
 class RandomErasing3(object):
-    """ Randomly selects a rectangle region in an image and erases its pixels.
-        'Random Erasing Data Augmentation' by Zhong et al.
-        See https://arxiv.org/pdf/1708.04896.pdf
-    Args:
-         probability: The probability that the Random Erasing operation will be performed.
-         sl: Minimum proportion of erased area against input image.
-         sh: Maximum proportion of erased area against input image.
-         r1: Minimum aspect ratio of erased area.
-         mean: Erasing value.
-    """
-
     def __init__(self, probability=0.5, sl=0.02, sh=0.4, r1=0.3,
                  mean=(0.4914, 0.4822, 0.4465)):
         self.probability = probability
@@ -111,13 +88,11 @@ class RandomErasing3(object):
         self.r1 = r1
 
     def __call__(self, img):
-        # img is Tensor [C,H,W]
         if random.uniform(0, 1) >= self.probability:
             return img, 0
 
         area = img.size(1) * img.size(2)
-
-        for attempt in range(100):
+        for _ in range(100):
             target_area = random.uniform(self.sl, self.sh) * area
             aspect_ratio = random.uniform(self.r1, 1 / self.r1)
 
@@ -134,22 +109,14 @@ class RandomErasing3(object):
                     img[2, x1:x1 + h, y1:y1 + w] = self.mean[2]
                 else:
                     img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
-
                 return img, 1
 
-        # if failed after 100 tries
         return img, 0
 
 
 def scheduler(optimizer, num_epochs: int = 120):
-    """
-    Cosine LR schedule.
-    Default num_epochs=120 (your HPC case).
-    You can pass args.epochs from training script if you want.
-    """
     lr_min = 0.002 * 0.008
     warmup_lr_init = 0.01 * 0.008
-
     warmup_t = 5
     noise_range = None
 
@@ -168,7 +135,6 @@ def scheduler(optimizer, num_epochs: int = 120):
         noise_std=1.,
         noise_seed=42,
     )
-
     return lr_scheduler
 
 
@@ -180,18 +146,14 @@ def optimizer(model):
         lr = 0.008
         weight_decay = 1e-4
         if "bias" in key:
-            lr = 0.008 * 2
+            lr = 0.016
             weight_decay = 1e-4
-
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
-    optimizer = getattr(torch.optim, 'SGD')(params, momentum=0.9)
-    return optimizer
+    return torch.optim.SGD(params, momentum=0.9)
 
 
 class Scheduler:
-    """ Parameter Scheduler Base Class """
-
     def __init__(self,
                  optimizer: torch.optim.Optimizer,
                  param_group_field: str,
@@ -204,6 +166,7 @@ class Scheduler:
         self.optimizer = optimizer
         self.param_group_field = param_group_field
         self._initial_param_group_field = f"initial_{param_group_field}"
+
         if initialize:
             for i, group in enumerate(self.optimizer.param_groups):
                 if param_group_field not in group:
@@ -213,6 +176,7 @@ class Scheduler:
             for i, group in enumerate(self.optimizer.param_groups):
                 if self._initial_param_group_field not in group:
                     raise KeyError(f"{self._initial_param_group_field} missing from param_groups[{i}]")
+
         self.base_values = [group[self._initial_param_group_field] for group in self.optimizer.param_groups]
         self.metric = None
         self.noise_range_t = noise_range_t
@@ -241,13 +205,6 @@ class Scheduler:
             values = self._add_noise(values, epoch)
             self.update_groups(values)
 
-    def step_update(self, num_updates: int, metric: float = None):
-        self.metric = metric
-        values = self.get_update_values(num_updates)
-        if values is not None:
-            values = self._add_noise(values, num_updates)
-            self.update_groups(values)
-
     def update_groups(self, values):
         if not isinstance(values, (list, tuple)):
             values = [values] * len(self.optimizer.param_groups)
@@ -255,28 +212,32 @@ class Scheduler:
             param_group[self.param_group_field] = value
 
     def _add_noise(self, lrs, t):
-        if self.noise_range_t is not None:
-            if isinstance(self.noise_range_t, (list, tuple)):
-                apply_noise = self.noise_range_t[0] <= t < self.noise_range_t[1]
-            else:
-                apply_noise = t >= self.noise_range_t
-            if apply_noise:
-                g = torch.Generator()
-                g.manual_seed(self.noise_seed + t)
-                if self.noise_type == 'normal':
-                    while True:
-                        noise = torch.randn(1, generator=g).item()
-                        if abs(noise) < self.noise_pct:
-                            break
-                else:
-                    noise = 2 * (torch.rand(1, generator=g).item() - 0.5) * self.noise_pct
-                lrs = [v + v * noise for v in lrs]
-        return lrs
+        if self.noise_range_t is None:
+            return lrs
+
+        if isinstance(self.noise_range_t, (list, tuple)):
+            apply_noise = self.noise_range_t[0] <= t < self.noise_range_t[1]
+        else:
+            apply_noise = t >= self.noise_range_t
+
+        if not apply_noise:
+            return lrs
+
+        g = torch.Generator()
+        g.manual_seed(self.noise_seed + t)
+
+        if self.noise_type == 'normal':
+            while True:
+                noise = torch.randn(1, generator=g).item()
+                if abs(noise) < self.noise_pct:
+                    break
+        else:
+            noise = 2 * (torch.rand(1, generator=g).item() - 0.5) * self.noise_pct
+
+        return [v + v * noise for v in lrs]
 
 
 class CosineLRScheduler(Scheduler):
-    """Cosine decay with restarts."""
-
     def __init__(self,
                  optimizer: torch.optim.Optimizer,
                  t_initial: int,
@@ -298,8 +259,6 @@ class CosineLRScheduler(Scheduler):
             noise_range_t=noise_range_t, noise_pct=noise_pct, noise_std=noise_std, noise_seed=noise_seed,
             initialize=initialize)
 
-        assert t_initial > 0
-        assert lr_min >= 0
         self.t_initial = t_initial
         self.t_mul = t_mul
         self.lr_min = lr_min
@@ -318,39 +277,31 @@ class CosineLRScheduler(Scheduler):
 
     def _get_lr(self, t):
         if t < self.warmup_t:
-            lrs = [self.warmup_lr_init + t * s for s in self.warmup_steps]
+            return [self.warmup_lr_init + t * s for s in self.warmup_steps]
+
+        if self.warmup_prefix:
+            t = t - self.warmup_t
+
+        if self.t_mul != 1:
+            i = math.floor(math.log(1 - t / self.t_initial * (1 - self.t_mul), self.t_mul))
+            t_i = self.t_mul ** i * self.t_initial
+            t_curr = t - (1 - self.t_mul ** i) / (1 - self.t_mul) * self.t_initial
         else:
-            if self.warmup_prefix:
-                t = t - self.warmup_t
+            i = t // self.t_initial
+            t_i = self.t_initial
+            t_curr = t - (self.t_initial * i)
 
-            if self.t_mul != 1:
-                i = math.floor(math.log(1 - t / self.t_initial * (1 - self.t_mul), self.t_mul))
-                t_i = self.t_mul ** i * self.t_initial
-                t_curr = t - (1 - self.t_mul ** i) / (1 - self.t_mul) * self.t_initial
-            else:
-                i = t // self.t_initial
-                t_i = self.t_initial
-                t_curr = t - (self.t_initial * i)
+        gamma = self.decay_rate ** i
+        lr_min = self.lr_min * gamma
+        lr_max_values = [v * gamma for v in self.base_values]
 
-            gamma = self.decay_rate ** i
-            lr_min = self.lr_min * gamma
-            lr_max_values = [v * gamma for v in self.base_values]
-
-            if self.cycle_limit == 0 or (self.cycle_limit > 0 and i < self.cycle_limit):
-                lrs = [
-                    lr_min + 0.5 * (lr_max - lr_min) * (1 + math.cos(math.pi * t_curr / t_i))
-                    for lr_max in lr_max_values
-                ]
-            else:
-                lrs = [self.lr_min for _ in self.base_values]
-        return lrs
+        if self.cycle_limit == 0 or (self.cycle_limit > 0 and i < self.cycle_limit):
+            return [lr_min + 0.5 * (lr_max - lr_min) * (1 + math.cos(math.pi * t_curr / t_i))
+                    for lr_max in lr_max_values]
+        else:
+            return [self.lr_min for _ in self.base_values]
 
     def get_epoch_values(self, epoch: int):
         if self.t_in_epochs:
             return self._get_lr(epoch)
-        return None
-
-    def get_update_values(self, num_updates: int):
-        if not self.t_in_epochs:
-            return self._get_lr(num_updates)
         return None
